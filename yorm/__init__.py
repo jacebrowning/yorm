1c494d45ac56644368b5b104cf3ce8e8301d8638
Jace Browning <jacebrowning@gmail.com>
2014-11-12 23:04:19 -0500

    Add 'container' to the package namespace

--- yorm/__init__.py
+++ yorm/__init__.py
@@ -8,7 +8,7 @@ __version__ = '0.2-dev'
 VERSION = __project__ + '-' + __version__
 
 try:
-    from . import standard, extended
+    from . import standard, extended, container
     from .utilities import UUID, store, store_instances, map_attr
     from .base import Mappable, Converter
 except ImportError:  # pragma: no cover (manual test)
