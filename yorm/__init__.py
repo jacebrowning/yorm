e67d7e910671f560e62cea5d885288f16230d0c5
Jace Browning <jacebrowning@gmail.com>
2015-01-18 12:04:00 -0500

    Add explicit version check

--- yorm/__init__.py
+++ yorm/__init__.py
@@ -7,6 +7,12 @@ __version__ = '0.3-dev'
 
 VERSION = __project__ + '-' + __version__
 
+PYTHON_VERSION = 3, 3
+
+import sys
+if not sys.version_info >= PYTHON_VERSION:  # pragma: no cover (manual test)
+    exit("Python {}.{}+ is required.".format(*PYTHON_VERSION))
+
 try:
     from . import standard, extended, container
     from .utilities import UUID, store, store_instances, map_attr
