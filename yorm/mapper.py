42c0e7ef888b56b9c32ea8d8338e2ef6b692e550
Jace Browning <jacebrowning@gmail.com>
2017-01-06 15:30:12 -0500

    Only convert container types

--- yorm/mapper.py
+++ yorm/mapper.py
@@ -207,15 +207,17 @@ class Mapper:
 
         # Add missing attributes
         for name, converter in attrs2.items():
-            if hasattr(self._obj, name):
-                existing_attr = getattr(self._obj, name)
-                if not isinstance(existing_attr, converter):
-                    log.trace("Converting attribute %r to %r", name, converter)
-                    value = converter.create_default()
-                    setattr(self._obj, name, value)
-                    self._remap(value, self)
+            existing_attr = getattr(self._obj, name, None)
+            if existing_attr:
+                if issubclass(converter, Container):
+                    if not isinstance(existing_attr, converter):
+                        msg = "Converting container attribute %r to %r"
+                        log.trace(msg, name, converter)
+                        value = converter.create_default()
+                        setattr(self._obj, name, value)
+                        self._remap(value, self)
             else:
-                value = converter.to_value(None)
+                value = converter.create_default()
                 msg = "Default value for missing object attribute: %s = %r"
                 log.warning(msg, name, value)
                 setattr(self._obj, name, value)
